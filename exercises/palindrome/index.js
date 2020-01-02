8c40493acc23db4a23ac1f5e4e243d32e80552e2
Valerii Kuzivanov <valerii.kuzivanov@gmail.com>
2020-01-02 18:46:45 +0300

    palindrome, reverse str

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,11 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+  for (let len = str.length; len > 0; len--) if ( str[left++] !== str[right--]) return false;
+  return true;
+}
 
 module.exports = palindrome;
